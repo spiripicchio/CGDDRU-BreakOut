6262dd168ed007db0a3a4aa1e10a54ab9b72dec6
Marco Bancale <etabubu@Sigismondo.local>
2018-11-29 15:43:33 +0000

    Final updates

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -6,6 +6,13 @@ public class Paddle : MonoBehaviour
 {
 	public float speed;
 
+	// AudioSource _audioSource;
+
+	// void Awake()
+	// {
+	// 	_audioSource = GetComponent<AudioSource>();
+	// }
+
 	// Use this for initialization
 	void Start() 
 	{
@@ -19,4 +26,13 @@ public class Paddle : MonoBehaviour
 		float position = transform.position.x + (Input.GetAxis("Horizontal") * speed * Time.deltaTime);
 		transform.position = new Vector3(Mathf.Clamp(position, -8f, 8f), -4, 0f);
 	}
+
+	// void OnCollisionEnter2D(Collision2D other)
+	// {
+	// 	Debug.Log(other.gameObject.tag);
+	// 	if (other.gameObject.tag == "Ball")
+	// 	{
+	// 		_audioSource.Play();
+	// 	}
+	// }
 }
